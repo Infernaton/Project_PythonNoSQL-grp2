5064184390e58c40d8ca8335bc436cdd769d82ba
Infernaton <lubin.pauly@gmail.com>
2021-10-19 15:28:00 +0200

    Modifier utilisateur

--- app.py
+++ app.py
@@ -29,6 +29,30 @@ def hello_world():
     return render_template('index.html', title='home', data=data)
 
 
+@app.route("/users/<id>", methods=["PATCH", "DELETE", "POST"])
+def modify_user(id):
+    error_code = "000"
+    error_message = "Success !"
+    if request.method == "PATCH":
+        json_data = request.json["modify"]
+        with open(f"users/{id}.txt", "r+") as f:
+            content = f.read().split("\n")
+            if json_data["firstname"] != "":
+                content[0] = json_data["firstname"]
+            if json_data["name"] != "":
+                content[1] = json_data["name"]
+            if json_data["age"] != "":
+                content[2] = json_data["age"]
+            f.seek(0)
+            f.write('\n'.join(content))
+
+    return {
+        "status": 200,
+        "error_code": error_code,
+        "error_message": error_message
+    }
+
+
 if __name__ == '__main__':
     app.run(
         host="0.0.0.0",
